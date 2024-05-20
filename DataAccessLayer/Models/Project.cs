2604df919538aa71f812b556b17602e63d30769e
kahol <kahola@gmail.com>
2024-05-20 14:15:02 +0200

    Add Project model in DataAccessLayer
    A Project record class has been added to the DataAccessLayer models. The Project model includes fields for Id, Title, Description, Content, and a List of Skills. Necessary attributes for validation such as Key, Required, and StringLength have been applied to the appropriate fields.

---
+++ DataAccessLayer/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models;
+
+public record Project
+{
+    [Key]
+    public int Id { get; init; }
+    [Required]
+    [StringLength(200, MinimumLength = 10)]
+    public string Title { get; init; } = null!;
+
+    public string Description { get; init; } = null!;
+    public string Content { get; init; } = null!;
+    public List<string> Skills { get; init; } = [];
+}
