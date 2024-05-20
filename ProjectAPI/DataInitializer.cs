e6e19e53a39d78b77717e7275ab4622ba0ea7924
kahol <kahola@gmail.com>
2024-05-20 14:14:17 +0200

    Add DataInitializer and update ApplicationDbContextModelSnapshot
    Implemented a new DataInitializer class to seed data into the Project database. Additionally, updated the ApplicationDbContextModelSnapshot to reflect the latest IdentityUser and IdentityRole models configurations within the Authentication system.

---
+++ ProjectAPI/DataInitializer.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectAPI;
+
+public class DataInitializer(ProjectDbContext context)
+{
+    public async Task SeedData()
+    {
+        await context.Database.MigrateAsync();
+        if (context.Projects.Any())
+            return;
+
+        var project = new Project
+        {
+            Title = "Hej",
+            Description = "HEEEJ",
+            Content = "HEEEEEEEJ"
+        };
+        context.Add(project);
+        await context.SaveChangesAsync();
+    }
+}
