e19bd856e63da043a5b552afe51951e3142776f3
kahol <kahola@gmail.com>
2024-05-20 14:15:10 +0200

    Add AutoMapper configuration for Project models
    Automapper configuration for Project and ProjectViewModel has been added in MappingProfile class. This will streamline the mapping between the two models, making data transfer easier and more efficient.

---
+++ ProjectAPI/Infrastructure/AutomapperConfigurations/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DataAccessLayer.Models;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.Infrastructure.AutomapperConfigurations;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Project, ProjectViewModel>().ReverseMap();
+    }
+}
