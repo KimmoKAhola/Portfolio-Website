15a1a990c1b53e885d739db49302cb56630a187e
kahol <kahola@gmail.com>
2024-05-20 14:15:51 +0200

    Add ProjectDbContextFactory for database context setup
    This commit introduces a new class 'ProjectDbContextFactory' in the DataAccessLayer. This class implements the 'IDesignTimeDbContextFactory<ProjectDbContext>' interface and it is responsible for creating and configuring the database context used for the application, utilizing a connection string provided by an appsettings.json.

---
+++ DataAccessLayer/ProjectDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer;
+
+public class ProjectDbContextFactory : IDesignTimeDbContextFactory<ProjectDbContext>
+{
+    public ProjectDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ProjectDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ProjectDbContext(optionsBuilder.Options);
+    }
+}
