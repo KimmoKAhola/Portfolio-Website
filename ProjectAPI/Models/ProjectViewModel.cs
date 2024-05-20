84adfcc88244eccc20b25d8010d804e42cdcbb39
kahol <kahola@gmail.com>
2024-05-20 14:16:06 +0200

    Add ProjectViewModel to project API
    This commit introduces a new model called ProjectViewModel in the ProjectAPI project. This model includes properties for Title, Description, Content, and a list of Skills, where Title is required and has a character length constraint of minimum 10 and maximum 200 characters.

---
+++ ProjectAPI/Models/ProjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectAPI.Models;
+
+public class ProjectViewModel
+{
+    [Required]
+    [StringLength(200, MinimumLength = 10)]
+    public string Title { get; init; } = null!;
+
+    public string Description { get; init; } = null!;
+    public string Content { get; init; } = null!;
+    public List<string> Skills { get; init; } = [];
+}
