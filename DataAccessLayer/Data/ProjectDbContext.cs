c2ecfe2232ed53eb101aae36ce738faf9e03d0a5
kahol <kahola@gmail.com>
2024-05-20 14:15:30 +0200

    Added ProjectDbContext class
    This commit introduces the ProjectDbContext class, which extends the DbContext class, in the DataAccessLayer. It also includes a DbSet property for the Project model.

---
+++ DataAccessLayer/Data/ProjectDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data;
+
+public class ProjectDbContext : DbContext
+{
+    protected ProjectDbContext()
+    {
+    }
+
+    public ProjectDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Project> Projects { get; set; }
+}
