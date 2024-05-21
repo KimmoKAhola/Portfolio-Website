59c4a3e9dd4b452fdfd9260e9b1566b7ab524735
kahol <kahola@gmail.com>
2024-05-21 10:22:56 +0200

    Rename ProjectViewModel and add new properties
    The class ProjectViewModel has been renamed to GithubProjectViewModel to better reflect its purpose. Additionally, two new properties, ProjectUrl and ImageUrl, have been added to the model.

--- ProjectAPI/Models/GithubProjectViewModel.cs
+++ ProjectAPI/Models/GithubProjectViewModel.cs
@@ -2,13 +2,14 @@
 
 namespace ProjectAPI.Models;
 
-public class ProjectViewModel
+public class GithubProjectViewModel
 {
     [Required]
     [StringLength(200, MinimumLength = 10)]
     public string Title { get; init; } = null!;
-
     public string Description { get; init; } = null!;
     public string Content { get; init; } = null!;
     public List<string> Skills { get; init; } = [];
+    public Uri ProjectUrl { get; init; } = null!;
+    public Uri ImageUrl { get; init; } = null!;
 }
