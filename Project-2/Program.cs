afae78b5b3d13e580a26951ba2428886857b5ae5
kahol <kahola@gmail.com>
2024-05-21 10:20:15 +0200

    Removed unused views and models, added new SVG icons, and updated program setup
    The unused views (Error, Privacy) and ErrorViewModel were removed as they're not needed in this project. Two new SVG icons were added for UI enhancements. Updates were made to Program.cs to reflect changes in the project setup that includes adding HttpClient services, changing the connection string, and rearranging the code for readability. Changes in the _ViewImports.cshtml file remove unnecessary using statements. Workspace configurations and commit

--- Project-2/Program.cs
+++ Project-2/Program.cs
@@ -1,19 +1,43 @@
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Project_2;
+using Project_2.Controllers;
 using Project_2.Data;
+using Project_2.Infrastructure;
+using Project_2.Models;
+using Project_2.Models.ViewModels.GithubProjects;
+using Project_2.Models.ViewModels.Weather;
+using Project_2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder
+    .Services.AddDefaultIdentity<IdentityUser>(options =>
+        options.SignIn.RequireConfirmedAccount = true
+    )
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+var apiKey = builder.Configuration.GetSection("ApiSettings")["Key"];
+
+builder.Services.AddHttpClient<IHttpService<Root>, WeatherApiService>(o =>
+{
+    // https://api.openweathermap.org/data/2.5/
+    o.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+    o.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+});
+
+builder.Services.AddHttpClient<IHttpService<GithubProjectModel>, GithubApiService>(o =>
+{
+    o.BaseAddress = new Uri("https://kimmo-github-api.azurewebsites.net/");
+});
 
 var app = builder.Build();
 
@@ -36,9 +60,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
