e4c3cce812716416ee05a2a6baafb8aef48c612d
kahol <kahola@gmail.com>
2024-05-21 10:22:10 +0200

    Add IndexViewModel in ViewModels
    A new file named IndexViewModel.cs has been added under the ViewModels directory. This class include WeatherModel, GithubProjectModel, ListOfSkills, and ContactMeModel which are some of the basic entities needed for our project. This ViewModel will help to simplify passing multiple entities to the view.

---
+++ Project-2/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Project_2.Models.FormModels.Contact;
+using Project_2.Models.ViewModels.GithubProjects;
+
+namespace Project_2.Models.ViewModels;
+
+public class IndexViewModel
+{
+    public WeatherModel WeatherModel { get; init; } = null!;
+    public List<GithubProjectModel> GithubProjectModels { get; init; } = null!;
+    public List<SkillModel> ListOfSkills { get; init; } = null!;
+    public ContactMeModel ContactMeModel { get; init; } = null!;
+}
