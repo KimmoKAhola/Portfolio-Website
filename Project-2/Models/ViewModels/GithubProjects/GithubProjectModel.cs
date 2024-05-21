42e0d5d86257ae8d3b3ed5ccee6ab6cfe5a2c66a
kahol <kahola@gmail.com>
2024-05-21 10:23:03 +0200

    Add GithubProjectModel to project
    A new model named GithubProjectModel was added to the Project-2 under GithubProjects directory. This model class includes properties such as Title, Description, Content, Skills, ProjectUrl, and ImageUrl.

---
+++ Project-2/Models/ViewModels/GithubProjects/GithubProjectModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Project_2.Models.ViewModels.GithubProjects;
+
+public class GithubProjectModel
+{
+    public string Title { get; init; } = null!;
+
+    public string Description { get; init; } = null!;
+    public string Content { get; init; } = null!;
+    public List<string> Skills { get; init; } = [];
+    public Uri ProjectUrl { get; init; } = null!;
+    public Uri ImageUrl { get; init; } = null!;
+}
