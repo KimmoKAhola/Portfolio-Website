d08173780c4578c98d7f38bda51c89fd72909d50
kahol <kahola@gmail.com>
2024-05-21 10:23:35 +0200

    Add ContactMeModel for handling contact form data
    A new model class, ContactMeModel, has been created under the Project-2 Models directory. This class will handle all input data from the site's contact form, including validation for fields such as FirstName, Surname, Email, and Message.

---
+++ Project-2/Models/FormModels/Contact/ContactMeModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_2.Models.FormModels.Contact;
+
+public class ContactMeModel
+{
+    [Required]
+    [StringLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+    public string FirstName { get; init; } = null!;
+
+    [Required]
+    [StringLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+    public string Surname { get; init; } = null!;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; } = null!;
+
+    [Required]
+    [StringLength(20, ErrorMessage = "Please enter at least 20 characters.")]
+    public string Message { get; init; } =
+        "Hej, du verkar verkligen vara den bästa programmeraren som inte har blivit anställd än. Vi från \"{ERT FÖRETAG}\" vill ha kontakt med dig omedelbums.";
+}
