74d90f7df1c1ce6e8385ebced5585ed77c2e4859
kahol <kahola@gmail.com>
2024-05-21 10:23:17 +0200

    Create GithubApiService class
    Added a new class GithubApiService to handle GitHub's API calls. The class includes methods for getting individual or all projects, with the GetAll method having implemented functionality. Exception handling included for unsuccessful API calls.

---
+++ Project-2/Services/GithubApiService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using Project_2.Infrastructure;
+using Project_2.Models.ViewModels.GithubProjects;
+
+namespace Project_2.Services;
+
+public class GithubApiService(HttpClient httpClient) : IHttpService<GithubProjectModel>
+{
+    private readonly JsonSerializerOptions _jsonOptions =
+        new() { PropertyNameCaseInsensitive = true };
+
+    public async Task<GithubProjectModel> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<GithubProjectModel> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<GithubProjectModel>> GetAll()
+    {
+        var httpResponse = await httpClient.GetAsync("Project");
+        try
+        {
+            httpResponse.EnsureSuccessStatusCode();
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<List<GithubProjectModel>>(
+                content,
+                _jsonOptions
+            );
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
