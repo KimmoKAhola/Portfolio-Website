deecd02d9b2e476eae45f71412060b963533b576
kahol <kahola@gmail.com>
2024-05-21 10:23:27 +0200

    Updated DataInitializer class with new GithubProject instances
    The DataInitializer class was updated to use GithubProject instances instead of the generic Project type. Two Initialized projects "Blackjack", and "Babun Bank" are now included with additional fields such as Skills, ProjectURL, and ImageURL populated. These project instances are saved to the context and provide clearer, more specific test data.

--- ProjectAPI/DataInitializer.cs
+++ ProjectAPI/DataInitializer.cs
@@ -12,13 +12,31 @@ public class DataInitializer(ProjectDbContext context)
         if (context.Projects.Any())
             return;
 
-        var project = new Project
+        var project = new GithubProject
         {
-            Title = "Hej",
-            Description = "HEEEJ",
-            Content = "HEEEEEEEJ"
+            Title = "Blackjack",
+            Description = "Good blackjack game",
+            Content = "Play until you lose all your money",
+            Skills = ["C#", "Console.WriteLine"],
+            ProjectURL = new Uri("https://github.com/KimmoKAhola/Blackjack"),
+            ImageURL = new Uri(
+                "https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA5L3JtNzA1ZGVzaWduLWVmZmVjdC10bnQtMDFlLmpwZw.jpg"
+            )
+        };
+
+        var project2 = new GithubProject
+        {
+            Title = "Babun Bank",
+            Description = "MVC bank project",
+            Content = "Can display users, accounts, customers. Login as admin, cashier.",
+            Skills = ["C#", "ASP.NET", "MVC", "RESTful API"],
+            ProjectURL = new Uri("https://github.com/KimmoKAhola/BabunBank"),
+            ImageURL = new Uri(
+                "https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA5L3JtNzA1ZGVzaWduLWVmZmVjdC10bnQtMDFlLmpwZw.jpg"
+            )
         };
         context.Add(project);
+        context.Add(project2);
         await context.SaveChangesAsync();
     }
 }
