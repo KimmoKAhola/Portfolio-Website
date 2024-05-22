b330f58236204aa75fa747a8142d9eb693ce5076
kahol <kahola@gmail.com>
2024-05-22 09:40:55 +0200

    Add ConfirmEmail field to ContactMeModel
    A new "ConfirmEmail" field was added to the ContactMeModel. This field is required and must match the entered "Email" field to validate correct user input and prevent typing mistakes.

--- Project-2/Models/FormModels/Contact/ContactMeModel.cs
+++ Project-2/Models/FormModels/Contact/ContactMeModel.cs
@@ -17,6 +17,10 @@ public class ContactMeModel
     public string Email { get; init; } = null!;
 
     [Required]
+    [Compare("Email")]
+    public string ConfirmEmail { get; init; } = null!;
+
+    [Required]
     [StringLength(20, ErrorMessage = "Please enter at least 20 characters.")]
     public string Message { get; init; } =
         "Hej, du verkar verkligen vara den bästa programmeraren som inte har blivit anställd än. Vi från \"{ERT FÖRETAG}\" vill ha kontakt med dig omedelbums.";
