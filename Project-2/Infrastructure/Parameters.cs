a38c71e2131d927a27b609ce7e6130f8bf10e991
kahol <kahola@gmail.com>
2024-05-22 09:41:53 +0200

    Updated Skills model and made Parameters class static
    The Parameters class has been modified to be a static class. The Skills model within the class has been enhanced to include an icon for each skill. The new icons are represented as URLs pointing to SVG images.

--- Project-2/Infrastructure/Parameters.cs
+++ Project-2/Infrastructure/Parameters.cs
@@ -2,21 +2,61 @@
 
 namespace Project_2.Infrastructure;
 
-public class Parameters
+public static class Parameters
 {
     public static readonly List<SkillModel> Skills =
     [
-        new SkillModel { SkillName = "C#", Description = "Best language" },
-        new SkillModel { SkillName = "JAVA", Description = "I spit on it" },
-        new SkillModel { SkillName = "Python", Description = "Love to hate it" },
-        new SkillModel { SkillName = "MS-SQL", Description = "I like databases" },
-        new SkillModel { SkillName = "Pandas", Description = "I like csv files" },
-        new SkillModel { SkillName = "MatPlotLib", Description = "Who doesn't like a good graph?" },
+        new SkillModel
+        {
+            SkillName = "C#",
+            Description = "Best language",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+                """
+        },
+        new SkillModel
+        {
+            SkillName = "JAVA",
+            Description = "I spit on it",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" />
+                """
+        },
+        new SkillModel
+        {
+            SkillName = "Python",
+            Description = "Love to hate it",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+                """
+        },
+        new SkillModel
+        {
+            SkillName = "MS-SQL",
+            Description = "I like databases",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
+                """
+        },
+        new SkillModel
+        {
+            SkillName = "Pandas",
+            Description = "I like csv files",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg" />
+                """
+        },
+        new SkillModel
+        {
+            SkillName = "MatPlotLib",
+            Description = "Who doesn't like a good graph?",
+            IconUrl = """
+                <img class="skill-icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg" />
+                """
+        },
         new SkillModel { SkillName = "Fluff", Description = "I ran out of skills" }
     ];
 
-    public static readonly Uri GitHub = new Uri("https://github.com/KimmoKAhola/");
-    public static readonly Uri LinkedIn = new Uri(
-        "https://www.linkedin.com/in/kimmo-ahola-0679b6290/"
-    );
+    public static readonly Uri GitHub = new("https://github.com/KimmoKAhola/");
+    public static readonly Uri LinkedIn = new("https://www.linkedin.com/in/kimmo-ahola-0679b6290/");
 }
