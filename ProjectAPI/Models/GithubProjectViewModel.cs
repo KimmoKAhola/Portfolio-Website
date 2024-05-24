28ba47dfbf4346ec5babaf181abc0400aa9e4298
kahol <kahola@gmail.com>
2024-05-24 14:17:01 +0200

    Remove unused code and refactor property names
    Several commented-out blocks of code were removed to improve readability. Additionally, the naming convention of multiple properties across models was unified for consistency, changing from 'Url' to 'URL'.

--- ProjectAPI/Models/GithubProjectViewModel.cs
+++ ProjectAPI/Models/GithubProjectViewModel.cs
@@ -10,7 +10,7 @@ public class GithubProjectViewModel
     public string Description { get; init; } = null!;
     public string Content { get; init; } = null!;
     public List<string> Skills { get; init; } = [];
-    public Uri ProjectUrl { get; init; } = null!;
-    public Uri ImageUrl { get; init; } = null!;
-    public Uri? LiveDemoUrl { get; init; }
+    public Uri ProjectURL { get; init; } = null!;
+    public Uri ImageURL { get; init; } = null!;
+    public Uri? LiveDemoURL { get; init; }
 }
