a07cbfe5b83154240daf2f3ad352734c02f9539f
kahol <kahola@gmail.com>
2024-05-24 12:31:40 +0200

    Add weather and GitHub project service interfaces
    The commit introduces new interfaces IGithubProjectService and IWeatherService to the infrastructure. It also adds a new model, Current, for weather data with the corresponding properties. Cleaned up workspace.xml and updated it to reflect the new changes.

---
+++ Project-2/Models/ViewModels/Weather/Current.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Project_2.Models.ViewModels.Weather;
+
+public class Current
+{
+    [JsonProperty("temperature_2m")]
+    [JsonPropertyName("temperature_2m")]
+    public double Temperature2m { get; set; }
+
+    [JsonProperty("interval")]
+    [JsonPropertyName("interval")]
+    public double Test { get; set; }
+
+    [JsonProperty("is_day")]
+    [JsonPropertyName("is_day")]
+    public int IsDay { get; set; }
+
+    [JsonProperty("weather_code")]
+    [JsonPropertyName("weather_code")]
+    public int WeatherCode { get; set; }
+}
